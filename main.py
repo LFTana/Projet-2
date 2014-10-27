7a4ec0708d6b37e1c9a53b684c244629dd2c7727
neelisn <neel.samat@eleve.egd.mg>
2014-10-27 16:43:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ x=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 y=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 x=(input("Entrez un nombre Héxadécimal : "))
 y=(input("Entrez un nombre Héxadécimal : "))
-z=(str(input("Entrez un type de calcul : ")))
-if z=="addition" :
+z=(str(object='addition'))
+if z=='addition' :
     w=y+x
 print(w)
