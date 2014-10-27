5b0bedd8a75278a936812cb0c7304459a4a9de6f
neelisn <neel.samat@eleve.egd.mg>
2014-10-27 16:39:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 ###Caculatrice Hexadécimale###
 ##############################
 
+w=0
 x=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 y=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-x=int(input("Entrez un nombre Héxadécimal: "))
-y=int(input("Entrez un nombre Héxadécimal: "))
-z=str(input('+', '-', '*', '/'))
-
-  
+x=(input("Entrez un nombre Héxadécimal : "))
+y=(input("Entrez un nombre Héxadécimal : "))
+z=(str(input("Entrez un type de calcul : ")))
+if z=="addition" :
+    w=y+x
+print(w)
