16a1b67787f29d76033d2a9eec45cb94be0071f2
neelisn <neel.samat@eleve.egd.mg>
2014-10-27 16:04:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ y=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 x=int(input("Entrez un nombre Héxadécimal: "))
 y=int(input("Entrez un nombre Héxadécimal: "))
 z=str(input('+', '-', '*', '/'))
-if z=='+' :
-  print (x+y)
+
   
