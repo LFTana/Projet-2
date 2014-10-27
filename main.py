349ab908d02081798adb021399dbe8623c046485
neelisn <neel.samat@eleve.egd.mg>
2014-10-27 15:47:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# ceci est le programme principal
-print("hello world")
+##############################
+###Caculatrice Hexadécimale###
+##############################
+
+x=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+y=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+x=int(input("Entrez un nombre Héxadécimal: "))
+y=int(input("Entrez un nombre Héxadécimal: "))
+z=str(input('+', '-', '*', '/'))
+if z=='+' :
+  print (x+y)
+  
