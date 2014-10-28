e81b7d86b972f7d3d81b4f90a87392fcb3bf1291
neelisn <neel.samat@eleve.egd.mg>
2014-10-28 10:26:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ w=0
 x=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 y=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 x=(input("Entrez un nombre Héxadécimal : "))
+z=(str(input("+ ou - ou * ou /")))
 y=(input("Entrez un nombre Héxadécimal : "))
-z=(str(object='addition'))
-if z=='addition' :
+if z=="+" :
     w=y+x
 print(w)
