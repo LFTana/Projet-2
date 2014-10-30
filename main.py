6c281953efe4b3de212c7c3a957e8820879b0d06
Nadjee <nadjee.saboor@eleve.egd.mg>
2014-10-30 15:59:58 +0300

    Update main.py
    Il faut encore modifier ce programme, au début il faudrait pouvoir entrer un nombre hexadécimal qui sera ensuite convertit en décimal. ce nombre décimal suivra ensuite le programme ci-dessus. Et à la fin le résultat en décimal doit être convertit en hexadécimal.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ w=0
 x=(int(input("Entrez un nombre Héxadécimal : ")))
 y=(int(input("Entrez un nombre Héxadécimal : ")))
 z=(str(input("Entrez un signe de calcul: ")))
-if z=="+" :
+if z=="+":
     w=x+y
-elif z=="*" :
+elif z=="*":
     w=y*x
 print(w)
