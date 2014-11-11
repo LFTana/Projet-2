f67ddd7c22bfc045cc50764201039d688141352a
neelisn <neel.samat@eleve.egd.mg>
2014-11-11 14:57:04 +0300

    Update main.py
    Je propose une petite piste pour avoir une idée de comment on peut se servir de la fonction " hex ( ) "
    Ce petit algorithme fonctionne. Mais, maintenant il faudrafusioner ce petit algorithme avec le grand et trouver : comment faire fonctionner cet algorithme avec des valeurs que l'utilisateur va insérer. C'est-à-dire qu'il faut remplacer le { ' A2 ' } par un { input("Entrez un nombre Héxadécimal : " } quelque part la dedans...

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 ###Caculatrice Hexadécimale###
 ##############################
 (Il faut utiliser hex() qui permet de convertir un nombre decimal en hexadecimal sur python)
+
+##############################
+#je propose une petite piste : 
+a = 'A2'                      
+b='0x'                        
+c=int(b+a,16)                 
+print(c)
+print(hex(c))
+##############################
+
 w=0
 x=((input("Entrez un nombre Héxadécimal : ")))
 # je dois pouvoir rentrer A2 par exemple comme nombre ...
