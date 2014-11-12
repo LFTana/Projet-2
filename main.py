4161cf5615142ca2f4655eeff1bb87963e90c2b2
Nadjee <nadjee.saboor@eleve.egd.mg>
2014-11-12 06:13:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 ##############################
 ###Caculatrice Hexadécimale###
 ##############################
-(Il faut utiliser hex() qui permet de convertir un nombre decimal en hexadecimal sur python)
 
-##############################
-#je propose une petite piste : 
-a = 'A2'                      
-b='0x'                        
-c=int(b+a,16)                 
-print(c)
-print(hex(c))
-##############################
 
-w=0
-x=((input("Entrez un nombre Héxadécimal : ")))
-# je dois pouvoir rentrer A2 par exemple comme nombre ...
-y=(int(input("Entrez un nombre Héxadécimal : ")))
-z=(str(input("Entrez un signe de calcul: ")))
-if z=="+":
-    w=x+y
-elif z=="*":
-    w=y*x
-print(w)
+b=((input("Entrez un nombre Héxadécimal : ")))
+c= '0x'
+d=int(c+b,16)
 
-print("hello")
+e=(str(input("Entrez un signe de calcul: ")))
+
+f=((input("Entrez un nombre Héxadécimal : ")))
+g=int(c+f,16)
+
+if e=="+":
+    a=d+g
+elif e=="*":
+    a=d*g
+
+print(hex(a))
+print((a))
