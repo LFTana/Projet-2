e3638c04838b8ed660b07c5f612739aee0875ec6
Ny Rado <andry.razafindrabe@eleve.egd.mg>
2014-11-12 10:43:23 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 ##############################
 
 b=((input("Entrez un nombre Héxadécimal : ")))  ##on entre le nomre hexadécimal à additionner ou multiplier## on le définit b##
-c= '0x'                     
-d=int(c+b,16)
+c= '0x'                                       ## on doit définir c= '0x' pour que pythoon comprenne que nous travaillons en hex##    
+d=int(c+b,16)                                 ##python commrend oxb, en base 16##
 
-e=(str(input("Entrez un signe de calcul: ")))
+e=(str(input("Entrez un signe de calcul: ")))  ##on définit la chaîne de calcul e##
 
-f=((input("Entrez un nombre Héxadécimal : ")))
+f=((input("Entrez un nombre Héxadécimal : ")))  ## on entre u la deuxieme valeur à additioner##
 g=int(c+f,16)
 
 if e=="+":
@@ -16,5 +16,5 @@ if e=="+":
 elif e=="*":
     a=d*g
 
-print(hex(a))
+print(hex(a))    # la commande hex nous permet de définir tous les termes nécessaires au calcul, on évite ainsi décrire tous les nombres en héxadécimal##
 print((a))
