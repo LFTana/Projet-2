566a62684745b025b9267665cbf46defad452a65
neelisn <neel.samat@eleve.egd.mg>
2014-11-16 14:20:28 +0300

    Update main.py
    j'ai pas la bonne version de python, en principe il n'y a pas d'erreur, 
    à vous de tester Nadjee ou Rado
    j'ai effectuer tous les commentaires aussi...

--- main.py
+++ main.py
@@ -2,21 +2,23 @@
 ###Caculatrice Hexadécimale###
 ##############################
 
-b=((input("Entrez un nombre Héxadécimal : ")))  ##on entre le nomre hexadécimal à additionner ou multiplier## on le définit b##
-c= '0x'                                       ## on doit définir c= '0x' pour que pythoon comprenne que nous travaillons en hex##    
-d=int(c+b,16)                                 ##python commrend oxb, en base 16##
+#Initialisation:
+b=((input("Entrez un nombre Héxadécimal : ")))  ## on entre un nombre hexadécimal, on le définit b
+d=int(b,16)                                     ## d prend la valeur b comme un nombre entier hexadécimal en base 16
 
-e=(str(input("Entrez un signe de calcul: ")))  ##on définit la chaîne de calcul e##
+e=(str(input("Entrez un signe de calcul, + ou * : ")))  ## on définit la chaîne de calcul e, e vaut + ou *
 
-f=((input("Entrez un nombre Héxadécimal : ")))  ## on entre u la deuxieme valeur à additioner##
-g=int(c+f,16)
+f=((input("Entrez un nombre Héxadécimal : ")))  ## on entre un deuxieme nombre hexadécimal, on le définit f
+g=int(f,16)                                     ## g prend la valeur f comme un nombre entier hexadécimal en base 16
 
-if e=="+":
-    a=d+g
-elif e=="*":
-    a=d*g
+#Traitement:
+if e=="+":                       ## Si e est le signe +
+    a=d+g                        ## alors les deux valeurs d et g vont s'additionner
+elif e=="*":                     ## Sinon si e est le signe *
+    a=d*g                        ## alors d et g vont se multiplier
 
-chr = str(hex(a))  
-chr=chr[2:100000000000000000]
+#Sortie:
+chr = str(hex(a))                 ## le résultat hexadécimal de a est considéré comme une chaîne : chr
+chr=chr[2:100000000000000000]     ## on décompose chr, on aura donc à l'affichage toutes les valeurs sauf les deux premières "0x"
 
-print("Le résultat vaut ",chr, " en Héxadécimal et ",a, " en Décimal.")    # la commande hex nous permet de définir tous les termes nécessaires au calcul, on évite ainsi d'écrire tous les nombres en héxadécimal##
+print("Le résultat vaut ",chr, " en Héxadécimal et ",a, " en Décimal.")    ## On affiche le résultat chr qui est en hexadécimal, et a qui est en décimal
